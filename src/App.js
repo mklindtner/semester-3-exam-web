fa540ac4bc78b15676a512a8a0837aae0eafd564
Thomas-Rosenkrans-Vestergaard <Thomas-Rosenkrans-Vestergaard@users.noreply.github.com>
2018-11-20 16:33:46 +0100

    authentication work

--- src/App.js
+++ src/App.js
@@ -11,15 +11,23 @@ class App extends Component {
 
   constructor(props) {
     super(props);
-    this.state = { authenticationContext: null }
+//    const authenticationContext = localStorage.getItem("authenticationContext");
+//    console.log(authenticationContext);
+//    if (authenticationContext !== null){
+//      console.log(authenticationContext);
+//     authenticationContext = JSON.parse(authenticationContext);
+//    }
+//    this.state = { authenticationContext: authenticationContext };
   }
 
-  onAuthentication = (user) => {
-    console.log({onAuthentication: user});
+  onAuthentication = (authenticationContext) => {
+//    const text = JSON.stringify({token: authenticationContext.token});
+//    localStorage.setItem("authenticationContext", text);
+    this.setState({ authenticationContext });
   }
 
   onRegistration = (user) => {
-    console.log({onRegistration: user});
+    console.log({ onRegistration: user });
   }
 
   render() {
@@ -28,7 +36,7 @@ class App extends Component {
         <Layout>
           <Route path="/registration" component={(router) => <RegistrationPage app={this.state} router={router} onRegistration={this.onRegistration} />} />
           <Route path="/authentication" component={(router) => <AuthenticationPage app={this.state} router={router} onAuthentication={this.onAuthentication} />} />
-          <Route path="/timeline" component={(router) => <Timeline app={this.state} router={router}/>} />
+          <Route path="/timeline" component={(router) => <Timeline app={this.state} router={router} />} />
         </Layout>
       </Router>
     );
