bb8041a24c98c0c1a0b9621a8767fcf842b3d09e
Thomas Vestergaard <Thomas-Rosenkrans-Vestergaard@users.noreply.github.com>
2018-11-26 00:20:24 +0100

    added working link to image grid

--- src/components/FriendGrid.js
+++ src/components/FriendGrid.js
@@ -10,7 +10,7 @@ class FriendGrid extends React.Component {
         <div className="row text-center text-lg-left">
           {this.props.friends.map(friend =>
             <div key={friend.id} className="col-lg-3 col-md-4 col-xs-6">
-              <a href="#" className="d-block mb-4 h-100">
+              <a href={"profile/" + friend.id} className="d-block mb-4 h-100">
                 <img className="img-fluid img-thumbnail" src={friend.profilePicture.src} alt={friend.name} />
                 <p>{friend.name}</p>
               </a>
