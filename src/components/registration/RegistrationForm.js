a5d2fbdd336848c6f9e3312db66035735346c581
Thomas Vestergaard <Thomas-Rosenkrans-Vestergaard@users.noreply.github.com>
2018-11-25 18:34:28 +0100

    added password repeat input

--- src/components/registration/RegistrationForm.js
+++ src/components/registration/RegistrationForm.js
@@ -18,16 +18,15 @@ class RegistrationForm extends Component {
             countries: [],
             regions: [],
             cities: [],
-
-            selectedName: "",
-            selectedGender: "",
-            selectedDateOfBirth: new Date().toISOString(),
-            selectedCountry: 0,
-            selectedRegion: 0,
-            selectedCity: 0,
-            selectedEmail: "",
-            selectedPassword: "",
-
+            name: "",
+            gender: "",
+            dateOfBirth: new Date().toISOString(),
+            country: 0,
+            region: 0,
+            city: 0,
+            email: "",
+            password: "",
+            passwordRepeat: "",
             errors: []
         }
 
@@ -60,11 +59,23 @@ class RegistrationForm extends Component {
         );
     }
 
+    onDefaultChange = (event) => {
+        const name = event.currentTarget.name;
+        const value = event.currentTarget.value;
+        this.setState({ [name]: value });
+    }
+
+    onGenderChange = (event) => {
+        event.persist();
+        const value = event.target.value;
+        this.setState({gender: value});
+    }
+
     onCountryChange = (event) => {
         event.persist();
         const value = event.target.value;
         this.locationMapper.getRegions(value).then(response => {
-            this.setState({ selectedCountry: value, cities: [], regions: response.body });
+            this.setState({ country: value, cities: [], regions: response.body });
         });
     }
 
@@ -72,31 +83,23 @@ class RegistrationForm extends Component {
         event.persist();
         const value = event.target.value;
         this.locationMapper.getCities(value).then(response => {
-            this.setState({ selectedRegion: value, cities: response.body });
+            this.setState({ region: value, cities: response.body });
         });
     }
 
     onCityChange = (event) => {
         event.persist();
         const value = event.target.value;
-        this.setState({ selectedCity: value });
+        this.setState({ city: value });
     }
 
-    onGenderChange = (event) => {
-        this.setState({ selectedGender: event.target.value });
-    }
-
-    onChangeDateOfBirth = (value) => {
-        this.setState({ selectedDateOfBirth: value });
-    }
-
-    onNameChange = (event) => {
-        this.setState({ selectedName: event.target.value });
+    onDateOfBirthChange = (value) => {
+        this.setState({dateOfBirth: value});
     }
 
     renderFirstStage = () => {
 
-        const { genders, countries, regions, cities, selectedName, selectedCountry, selectedRegion, selectedCity, selectedGender, selectedDateOfBirth, errors } = this.state;
+        const { genders, countries, regions, cities, errors } = this.state;
 
         return (
             <FormGroup controlId="registration-1">
@@ -104,15 +107,16 @@ class RegistrationForm extends Component {
                     id="formControlsName"
                     type="text"
                     label="Name"
+                    name="name"
                     minLength={0}
                     maxLength={255}
                     placeholder="Please enter your name."
-                    value={selectedName}
-                    onChange={this.onNameChange}
+                    value={this.state.name}
+                    onChange={this.onDefaultChange}
                 />
                 <FormGroup controlId="formControlsCountry">
                     <ControlLabel>Gender</ControlLabel>
-                    <FormControl value={selectedGender} componentClass="select" placeholder="Select your gender" onChange={this.onGenderChange}>
+                    <FormControl value={this.state.gender} componentClass="select" placeholder="Select your gender" onChange={this.onGenderChange}>
                         <option defaultValue={true}>Select your gender.</option>
                         {genders.map(gender =>
                             <option key={gender} value={gender}>{gender}</option>
@@ -121,11 +125,11 @@ class RegistrationForm extends Component {
                 </FormGroup>
                 <FormGroup>
                     <ControlLabel>Date of birth</ControlLabel>
-                    <DatePicker id="example-datepicker" value={selectedDateOfBirth} onChange={this.onChangeDateOfBirth} />
+                    <DatePicker id="example-datepicker" value={this.state.dateOfBirth} onChange={this.onDateOfBirthChange} />
                 </FormGroup>
                 <FormGroup controlId="formControlsCountry">
                     <ControlLabel>Country</ControlLabel>
-                    <FormControl value={selectedCountry} componentClass="select" placeholder="Select your country" onChange={this.onCountryChange}>
+                    <FormControl value={this.state.country} componentClass="select" placeholder="Select your country" onChange={this.onCountryChange}>
                         <option>Select your country.</option>
                         {countries.map(country =>
                             <option key={country.id} value={country.id}>{country.name}</option>
@@ -134,7 +138,7 @@ class RegistrationForm extends Component {
                 </FormGroup>
                 {regions.length > 0 && <FormGroup controlId="formControlsRegion">
                     <ControlLabel>Region</ControlLabel>
-                    <FormControl value={selectedRegion} componentClass="select" placeholder="Select your region" onChange={this.onRegionChange}>
+                    <FormControl value={this.state.region} componentClass="select" placeholder="Select your region" onChange={this.onRegionChange}>
                         <option>Select your region.</option>
                         {regions.map(region =>
                             <option key={region.id} value={region.id}>{region.name}</option>
@@ -143,7 +147,7 @@ class RegistrationForm extends Component {
                 </FormGroup>}
                 {cities.length > 0 && <FormGroup controlId="formControlsCity">
                     <ControlLabel>City</ControlLabel>
-                    <FormControl value={selectedCity} componentClass="select" placeholder="Select your city" onChange={this.onCityChange}>
+                    <FormControl value={this.state.city} componentClass="select" placeholder="Select your city" onChange={this.onCityChange}>
                         <option>Select your city.</option>
                         {cities.map(city =>
                             <option key={city.id} value={city.id}>{city.name}</option>
@@ -155,22 +159,15 @@ class RegistrationForm extends Component {
         );
     }
 
-    onEmailChange = (event) => {
-        this.setState({ selectedEmail: event.target.value });
-    }
-
-    onPasswordchange = (event) => {
-        this.setState({ selectedPassword: event.target.value });
-    }
-
     onSubmit = () => {
         const user = {
-            name: this.state.selectedName,
-            email: this.state.selectedEmail,
-            password: this.state.selectedPassword,
-            city: this.state.selectedCity,
-            gender: this.state.selectedGender,
-            dateOfBirth: new Date(this.state.selectedDateOfBirth).toISOString().substring(0, 10),
+            name: this.state.name,
+            email: this.state.email,
+            password: this.state.password,
+            passwordRepeat: this.state.passwordRepeat,
+            city: this.state.city,
+            gender: this.state.gender,
+            dateOfBirth: new Date(this.state.dateOfBirth).toISOString().substring(0, 10),
         }
 
         this.userMapper.create(user).then(response => {
@@ -208,8 +205,9 @@ class RegistrationForm extends Component {
                     minLength={0}
                     maxLength={255}
                     placeholder="Please enter your email address."
-                    value={this.state.selectedEmail}
-                    onChange={this.onEmailChange}
+                    name="email"
+                    value={this.state.email}
+                    onChange={this.onDefaultChange}
                 />
                 <FieldGroup
                     id="formControlsPassword"
@@ -217,12 +215,24 @@ class RegistrationForm extends Component {
                     label="Password"
                     minLength={0}
                     maxLength={255}
+                    name="password"
                     placeholder="Please choose a password."
-                    value={this.state.selectedPassword}
-                    onChange={this.onPasswordchange}
+                    value={this.state.password}
+                    onChange={this.onDefaultChange}
+                />
+                <FieldGroup
+                    id="formControlsPasswordRepeat"
+                    type="password"
+                    label="Repeat Password"
+                    name="passwordRepeat"
+                    minLength={0}
+                    maxLength={255}
+                    placeholder="Please choose a password."
+                    value={this.state.passwordRepeat}
+                    onChange={this.onDefaultChange}
                 />
                 <Button onClick={() => this.setState({ stage: this.state.stage - 1 })}>Previous</Button>
-                <Button onClick={this.onSubmit}>Register account</Button>
+                <Button style={{marginLeft: '20px'}} onClick={this.onSubmit}>Register account</Button>
             </FormGroup>
         )
     }
