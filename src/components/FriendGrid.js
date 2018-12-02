e6e2dddae85a0e9479919408191aef957a54ead8
Kristoffer Tolboll <Kristofferthetolboll@gmail.com>
2018-12-02 21:16:59 +0100

    fixed error in FriendGrid

--- src/components/FriendGrid.js
+++ src/components/FriendGrid.js
@@ -2,12 +2,12 @@ import React from "react";
 import './FriendGrid.css';
 import LargeProfilePicture from '../components/images/LargeProfilePicture';
 
-const friendGrid  = () => {
+const friendGrid  = (props) => {
 
     return (
       <div className="col-12 friend-grid-container">
         <div className="row text-center text-lg-left">
-          {this.props.friends.map(friend =>
+          {props.friends.map(friend =>
             <div key={friend.id} className="col-lg-3 col-md-4 col-xs-6">
               <a href={"profile/" + friend.id} className="d-block mb-4 h-100">
                 <LargeProfilePicture user={friend} width="200px" height="200px"/>
