6eb276a0eb020629aff9ef1667491d608cde527e
Kristoffer Tolboll <Kristofferthetolboll@gmail.com>
2018-12-03 20:58:35 +0100

    rettede respektive navne fra rest delen

--- src/data/CommentMapper.js
+++ src/data/CommentMapper.js
@@ -4,21 +4,21 @@ import {get, post, deleter} from './DataMapper';
 class CommentMapper {
 
     createPostComment(postId, contents){
-        return post(config.restUrl + `posts/${postId}/comments`, {
+        return post(config.restUrl + `comments/${postId}/`, {
             contents
         });
     }
 
     getPostComments(postId, pageSize, pageNumber) {
-        return get(config.restUrl + `posts/${postId}/comments/page/${pageSize}/${pageNumber}`);
+        return get(config.restUrl + `comments/${postId}/page/${pageSize}/${pageNumber}`);
     }
 
-    deletePostComments(postId, commentId){
-        return deleter(config.restUrl + `posts/${postId}/comments/${commentId}`);
+    deletePostComments(commentId){
+        return deleter(config.restUrl + `comments/${commentId}`);
     }
 
     getPostCommentsCount(postId){
-        return get(config.restUrl + `posts/${postId}/comments/count`).then(response => {
+        return get(config.restUrl + `commentparent/${postId}/count`).then(response => {
             return response.status === 200 ? Promise.resolve(response.body.count) : -1;
         });
     }
