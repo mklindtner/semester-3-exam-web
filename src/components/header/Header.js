5a68587436835e6cf607a9b459077702603ee5e5
Jonas <jonasgroenbek@gmail.com>
2018-12-05 00:31:47 +0100

    made the header responsive with mediaqueries and bootstrap grid

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -29,11 +29,6 @@ class Header extends Component {
     window.removeEventListener("resize", this.updateDimensions);
   };
 
-  toggle = () => {
-    const node = this.myRef.current;
-    node.classList.toggle("change");
-  };
-
   render() {
     return (
       <header>
@@ -42,52 +37,42 @@ class Header extends Component {
             if (matches) {
               return (
                 <div>
-                  <div className="col-xs-8 ">
-                    <UserSearch />
+                  <div className="search-input col-xs-9">
+                    <div className="search-input-max">
+                      <UserSearch />
+                    </div>
                   </div>
-                  <ul className="nav navbar-nav navbar-right">
-                    {this.props.app.authenticationContext != null && (
-                      <li className="dropdown" id="user-status">
-                        <a
-                          href="#"
-                          className="dropdown-toggle"
-                          data-toggle="dropdown"
-                          role="button"
-                          aria-expanded="false"
-                        >
-                          <span>
-                            <div
-                              class="container dropdown-toggle"
-                              ref={this.myRef}
-                              onClick={this.toggle}
-                            >
-                              <div class="bar1" />
-                              <div class="bar2" />
-                              <div class="bar3" />
-                            </div>
-                          </span>
-                        </a>
-                        <ul className="dropdown-menu" role="menu">
-                          <li>
-                            <Link to="/friend-requests">Friend requests</Link>
-                          </li>
-                          <li>
-                            <Link to="/profile">Profile</Link>
-                          </li>
-                          <li>
-                            <Link to="/settings">Settings</Link>
-                          </li>
-                          <li>
-                            <Link to="/timeline">Timeline</Link>
-                          </li>
-                          <li className="divider" />
-                          <li onClick={this.props.onLogout}>
-                            <a href="#">Log out</a>
-                          </li>
-                        </ul>
+                  <div className="btn-group col-xs-3">
+                    <button
+                      class="btn btn-default dropdown-toggle"
+                      type="button"
+                      id="dropdownMenu"
+                      data-toggle="dropdown"
+                      aria-haspopup="true"
+                      aria-expanded="false"
+                    />
+                    <ul
+                      className="dropdown-menu"
+                      aria-labelledby="dropdownMenu"
+                    >
+                      <li>
+                        <Link to="/friend-requests">Friend requests</Link>
                       </li>
-                    )}
-                  </ul>
+                      <li>
+                        <Link to="/profile">Profile</Link>
+                      </li>
+                      <li>
+                        <Link to="/settings">Settings</Link>
+                      </li>
+                      <li>
+                        <Link to="/timeline">Timeline</Link>
+                      </li>
+                      <li className="divider" />
+                      <li onClick={this.props.onLogout}>
+                        <a href="#">Log out</a>
+                      </li>
+                    </ul>
+                  </div>
                 </div>
               );
             } else {
@@ -95,56 +80,47 @@ class Header extends Component {
             }
           }}
         </MediaQuery>
-        <MediaQuery maxWidth={850} minWidth={470}>
+        <MediaQuery maxWidth={850} minWidth={471}>
           {matches => {
             if (matches) {
               return (
                 <div>
-                  <nav id="header-nav">
-                    <div className="left col-s-2">
-                      <a className="nav-link" href="/timeline">
-                        Timeline
-                      </a>
-                    </div>
-                    <div className="left search-input col-s-6">
-                      <UserSearch />
-                    </div>
-                    <div className="right user-status">
-                      <ul className="nav navbar-nav navbar-right">
-                        {this.props.app.authenticationContext != null && (
-                          <li className="dropdown" id="user-status">
-                            <a
-                              href="#"
-                              className="dropdown-toggle"
-                              data-toggle="dropdown"
-                              role="button"
-                              aria-expanded="false"
-                            >
-                              {this.props.app.authenticationContext.user.name}
-                              <span className="caret" />
-                            </a>
-                            <ul className="dropdown-menu" role="menu">
-                              <li>
-                                <Link to="/friend-requests">
-                                  Friend requests
-                                </Link>
-                              </li>
-                              <li>
-                                <Link to="/profile">Profile</Link>
-                              </li>
-                              <li>
-                                <Link to="/settings">Settings</Link>
-                              </li>
-                              <li className="divider" />
-                              <li onClick={this.props.onLogout}>
-                                <a href="#">Log out</a>
-                              </li>
-                            </ul>
-                          </li>
-                        )}
-                      </ul>
-                    </div>
-                  </nav>
+                  <a id="timeline" className="col-xs-2" href="/timeline">
+                    Timeline
+                  </a>
+                  <div className="search-input col-xs-5">
+                    <UserSearch />
+                  </div>
+                  <div className="text-right btn-group col-xs-5 pull-right">
+                    <button
+                      type="button"
+                      className="btn-link btn-default dropdown-toggle"
+                      data-toggle="dropdown"
+                      aria-expanded="false"
+                      name="test"
+                    >
+                      {this.props.app.authenticationContext.user.name}
+                      <span className="caret" />
+                    </button>
+                    <ul
+                      className="dropdown-menu"
+                      aria-labelledby="dropdownMenu"
+                    >
+                      <li>
+                        <Link to="/friend-requests">Friend requests</Link>
+                      </li>
+                      <li>
+                        <Link to="/profile">Profile</Link>
+                      </li>
+                      <li>
+                        <Link to="/settings">Settings</Link>
+                      </li>
+                      <li className="divider" />
+                      <li onClick={this.props.onLogout}>
+                        <a href="#">Log out</a>
+                      </li>
+                    </ul>
+                  </div>
                 </div>
               );
             } else {
@@ -157,56 +133,45 @@ class Header extends Component {
             if (matches) {
               return (
                 <div>
-                  <nav id="header-nav">
-                    <div className="left">
-                      <a className="nav-link" href="#">
-                        Social
-                      </a>
-                    </div>
-                    <div className="left">
-                      <a className="nav-link" href="/timeline">
-                        Timeline
-                      </a>
-                    </div>
-                    <div className="left search-input col-7">
-                      <UserSearch />
-                    </div>
-                    <div className="right user-status">
-                      <ul className="nav navbar-nav navbar-right">
-                        {this.props.app.authenticationContext != null && (
-                          <li className="dropdown" id="user-status">
-                            <a
-                              href="#"
-                              className="dropdown-toggle"
-                              data-toggle="dropdown"
-                              role="button"
-                              aria-expanded="false"
-                            >
-                              {this.props.app.authenticationContext.user.name}
-                              <span className="caret" />
-                            </a>
-                            <ul className="dropdown-menu" role="menu">
-                              <li>
-                                <Link to="/friend-requests">
-                                  Friend requests
-                                </Link>
-                              </li>
-                              <li>
-                                <Link to="/profile">Profile</Link>
-                              </li>
-                              <li>
-                                <Link to="/settings">Settings</Link>
-                              </li>
-                              <li className="divider" />
-                              <li onClick={this.props.onLogout}>
-                                <a href="#">Log out</a>
-                              </li>
-                            </ul>
-                          </li>
-                        )}
-                      </ul>
-                    </div>
-                  </nav>
+                  <a id="social" className="col-sm-1" href="#">
+                    Social
+                  </a>
+                  <a id="timeline" className="col-sm-1" href="/timeline">
+                    Timeline
+                  </a>
+                  <div className="search-input col-sm-5">
+                    <UserSearch />
+                  </div>
+                  <div className="text-right btn-group col-xs-5 pull-right">
+                    <button
+                      type="button"
+                      className="btn-link btn-default dropdown-toggle"
+                      data-toggle="dropdown"
+                      aria-haspopup="true"
+                      aria-expanded="false"
+                    >
+                      {this.props.app.authenticationContext.user.name}
+                      <span className="caret" />
+                    </button>
+                    <ul
+                      className="dropdown-menu"
+                      aria-labelledby="dropdownMenu"
+                    >
+                      <li>
+                        <Link to="/friend-requests">Friend requests</Link>
+                      </li>
+                      <li>
+                        <Link to="/profile">Profile</Link>
+                      </li>
+                      <li>
+                        <Link to="/settings">Settings</Link>
+                      </li>
+                      <li className="divider" />
+                      <li onClick={this.props.onLogout}>
+                        <a href="#">Log out</a>
+                      </li>
+                    </ul>
+                  </div>
                 </div>
               );
             } else {
