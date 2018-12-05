e251f9bc280a42987eb9a5c52b9aeb6be18a6d04
Thomas-Rosenkrans-Vestergaard <Thomas-Rosenkrans-Vestergaard@users.noreply.github.com>
2018-12-05 16:07:36 +0100

    added rolling chat messages

--- src/components/chat/ChatWindow.js
+++ src/components/chat/ChatWindow.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import getAuthenticationContext from "../../getAuthenticationContext";
 import ChatMapper from '../../data/ChatMapper';
 import './ChatWindow.css';
+import SmallProfilePicture from "../images/SmallProfilePicture";
 
 export default class ChatWindow extends Component {
 
@@ -12,12 +13,28 @@ export default class ChatWindow extends Component {
         this.socket.onopen = () => this.send("authentication", { token: getAuthenticationContext().token });
         this.socket.onmessage = message => this.handleIncoming(JSON.parse(message.data));
         this.state = { friends: [], searchedFriends: [], show: null, messages: [] };
+        this.userMap = {};
+        this.userMap[getAuthenticationContext().user.id] = {
+            user: getAuthenticationContext().user
+        }
+
+        this.fetchingMore = false;
+        this.hasMore = true;
+    }
+
+    appendTextMessage = (payload) => {
+        if (this.state.show != null) {
+            if (this.state.show.user.id == payload.sender) {
+                this.setState({ messages: this.state.messages.concat(payload) });
+            }
+        }
     }
 
     handleIncoming = (message) => {
+
         const type = message.type;
-        console.log(message);
         if (type == 'message') {
+            this.appendTextMessage(message.payload);
             return;
         }
 
@@ -25,7 +42,6 @@ export default class ChatWindow extends Component {
             this.updateFriends(friend => {
                 if (friend.user.id == message.payload.user) {
                     friend.online = true;
-                    console.log(friend);
                 }
                 return friend;
             });
@@ -36,7 +52,6 @@ export default class ChatWindow extends Component {
             this.updateFriends(friend => {
                 if (friend.user.id == message.payload.user) {
                     friend.online = false;
-                    console.log(friend);
                 }
                 return friend;
             });
@@ -47,7 +62,6 @@ export default class ChatWindow extends Component {
     updateFriends = (f) => {
         const friends = this.state.friends.map(f);
         const searchedFriends = this.state.searchedFriends.map(f);
-
         this.setState({ friends, searchedFriends });
     }
 
@@ -59,6 +73,9 @@ export default class ChatWindow extends Component {
         this.chatMapper.getFriends().then(response => {
             if (response.status === 200) {
                 this.setState({ friends: response.body, searchedFriends: response.body });
+                response.body.map(friend => {
+                    this.userMap[friend.user.id] = friend;
+                });
             }
         })
     }
@@ -76,7 +93,12 @@ export default class ChatWindow extends Component {
     show = (friend) => {
         this.chatMapper.getHistory(friend.user.id, 25, null).then(response => {
             if (response.status === 200) {
-                this.setState({ show: friend, messages: response.body });
+                this.setState({ show: friend, messages: response.body }, () => {
+                    var container = document.getElementById("chat-messages");
+                    container.scrollTop = container.scrollHeight;
+                    this.fetchingMore = false;
+                    this.hasMore = true;
+                });
                 return;
             }
 
@@ -92,17 +114,53 @@ export default class ChatWindow extends Component {
         e.preventDefault();
 
         const form = e.currentTarget;
-        const message = form.message.value;
-        this.send("text", {
-            contents: message,
-            receiver: user.id
+        const message = {
+            contents: form.message.value,
+            receiver: user.id,
+            sender: getAuthenticationContext().user.id
+        };
+        this.send("text", message);
+        this.setState({ messages: this.state.messages.concat(message) })
+    }
+
+    fetchMore = (container) => {
+
+        if (this.fetchingMore || !this.hasMore)
+            return;
+
+        const message = this.state.messages[0];
+        if (message == undefined)
+            return;
+
+        this.fetchingMore = true;
+        const beforeHeight = container.scrollHeight;
+        this.chatMapper.getHistory(this.state.show.user.id, 25, message.id).then(response => {
+    
+            if (response.status === 200) {
+                this.setState({ messages: response.body.concat(this.state.messages) }, () => {
+                    if (response.body.length == 0)
+                        this.hasMore = false;
+                    const afterHeight = container.scrollHeight;
+                    container.scrollTop = container.scrollTop + afterHeight - beforeHeight;
+                });
+
+                this.fetchingMore = false;
+            }
+
         });
+    }
+
+    onScroll = (e) => {
+        const scroll = e.currentTarget.scrollTop;
+        if (scroll < 600) {
+            this.fetchMore(e.currentTarget);
+        }
     }
 
     render() {
 
         return (
-            <div className="chat-container">
+            <div className="chat-container" >
                 <ul className="chat-header">
                     {!this.state.show && <li>Your friends</li>}
                     {this.state.show && <li className="chat-control" onClick={this.showFriends}>Back</li>}
@@ -128,21 +186,31 @@ export default class ChatWindow extends Component {
                     {this.state.show &&
                         <div className="chat-friend">
                             <div className="chat-messages-container col-xl-auto">
-                                <ul className="chat-messages-list">
-                                    <li className="chat-messages-item">
-
-                                    </li>
+                                <ul id="chat-messages" className="chat-messages" onScroll={this.onScroll}>
+                                    {this.state.messages.length < 1 &&
+                                        <li className="chat-message">
+                                            <p style={{ fontStyle: 'italic' }} className="chat-message-contents">No messages</p>
+                                        </li>}
+                                    {this.state.messages.map(message =>
+                                        <li className={getAuthenticationContext().user.id == message.sender ? "chat-message sender" : "chat-message receiver"}>
+                                            <div className="chat-message-profile">
+                                                <SmallProfilePicture user={this.userMap[message.sender].user} />
+                                                <p className="chat-message-author">{message.id} + {this.userMap[message.sender].user.name}</p>
+                                            </div>
+                                            <p className="chat-message-contents">{message.contents}</p>
+                                        </li>
+                                    )}
                                 </ul>
                             </div>
                             <div className="chat-input-container">
                                 <form className="chat-input-form" onSubmit={e => this.onSendMessage(e, this.state.show.user)} >
-                                    <input name="message" className="chat-input-message" type="text" />
+                                    <textarea placeholder="Send a message" name="message" className="chat-input-message" type="text" />
                                     <input name="submit" className="chat-input-submit" type="submit" />
                                 </form>
                             </div>
                         </div>}
                 </div>
-            </div>
+            </div >
         )
     }
 }
